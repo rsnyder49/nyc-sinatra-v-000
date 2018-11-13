94a73b20be20884486e4277af47550a811826afd
Robert Snyder <r.snyder49@gmail.com>
2018-11-13 19:30:18 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,50 @@
 class FiguresController < ApplicationController
-  # add controller methods
+  
+  get '/figures' do
+    @figures = Figure.all
+    
+    erb :'figures/index'
+  end
+  
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+  
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    
+    erb :'figures/show'
+  end
+  
+  post '/figures' do
+    @figure = Figure.new(params[:figure])
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    @figure.save
+     redirect to "figures/#{@figure.id}"
+  end
+   
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    
+     erb :'figures/edit'
+  end
+  
+  patch '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+    if !params[:landmark][:name].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+    @figure.save
+     redirect to "/figures/#{@figure.id}"
+  end
+   
 end
